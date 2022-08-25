a20122e74547b61f3f86f43cc48e177e795ece5a
Joyce Fernandes <joycecode@hotmail.com>
2022-08-25 10:26:52 +0200

    process.env.PORT on server.cjs

--- server.cjs
+++ server.cjs
@@ -1,7 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 
-const PORT = 8081
+const PORT = process.env.PORT || 8081
 
 fs.readFile('./index.html', function (err, html){
     if (err) throw err;
