73294d243971722994bcbf8708478ef73c35375a
Joyce Fernandes <joycecode@hotmail.com>
2022-08-27 13:49:13 +0200

    change line 38

--- index.js
+++ index.js
@@ -2,29 +2,15 @@ import express from 'express'
 import { dirname } from 'path'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import dotenv from 'dotenv'
-
-dotenv.config({ path: '.env' })
 
 const initServer = () => {
   const port = 3000
-
-  console.log(port, 'port')
 
   const app = express()
 
   const __filename = fileURLToPath(import.meta.url);
 
-  console.log('__filename', __filename)
-
-  console.log('.env', process.env.ROUTE_BASE)
-
-  const __dirname = path.dirname(`
-    ${process.env.ROUTE_BASE}
-    ${__filename}
-  `)
-
-  console.log('__dirname', __dirname)
+  const __dirname = path.dirname(__filename);
 
   app.use(express.static(__dirname))
 
@@ -33,12 +19,8 @@ const initServer = () => {
   })
 
   app.get('/', (req, res) => {
-    console.log('before-render'. __dirname)
-
-    res.sendFile(path.join(__dirname, ''))
+    res.sendFile('index.html', { root: __dirname } )
   })
 }
-
-console.log('before-func **********************')
 
 initServer()
