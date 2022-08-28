03430e3ded898d1bd8c8142a0931e82ca4c51dbe
Joyce Fernandes <joycecode@hotmail.com>
2022-08-28 15:26:22 +0200

    New commit

--- public/bundle.js
+++ public/bundle.js
@@ -1,56 +1,56 @@
 const projects = [
 
-  {
-    name: "Project 1",
-    id: "1",
-    url: "../ifc/01.ifc",
-  },
-  {
-    name: "Project 2",
-    id: "2",
-    url: "../ifc/02.ifc",
-  },
-  {
-    name: "Project 3",
-    id: "3",
-    url: "../ifc/03.ifc",
-  },
-  {
-    name: "Project 4",
-    id: "4",
-    url: "../ifc/04.ifc",
-  },
-  {
-    name: "Project 5",
-    id: "5",
-    url: "../ifc/05.ifc",
-  },
-  {
-    name: "Project 6",
-    id: "1",
-    url: "../ifc/01.ifc",
-  },
-  {
-    name: "Project 7",
-    id: "2",
-    url: "../ifc/02.ifc",
-  },
-  {
-    name: "Project 8",
-    id: "3",
-    url: "../ifc/03.ifc",
-  },
-  {
-    name: "Project 9",
-    id: "4",
-    url: "../ifc/04.ifc",
-  },
-  {
-    name: "Project 10",
-    id: "5",
-    url: "../ifc/05.ifc",
-  },
-];
+	{
+	  name: "Project 1",
+	  id: "1",
+	  url: "../ifc/01.ifc",
+	},
+	{
+	  name: "Project 2",
+	  id: "2",
+	  url: "../ifc/02.ifc",
+	},
+	{
+	  name: "Project 3",
+	  id: "3",
+	  url: "../ifc/03.ifc",
+	},
+	{
+	  name: "Project 4",
+	  id: "4",
+	  url: "../ifc/04.ifc",
+	},
+	{
+	  name: "Project 5",
+	  id: "5",
+	  url: "../ifc/05.ifc",
+	},
+	{
+	  name: "Project 6",
+	  id: "6",
+	  url: "../ifc/06.ifc",
+	},
+	{
+	  name: "Project 7",
+	  id: "7",
+	  url: "../ifc/07.ifc",
+	},
+	{
+	  name: "Project 8",
+	  id: "8",
+	  url: "../ifc/08.ifc",
+	},
+	{
+	  name: "Project 9",
+	  id: "9",
+	  url: "../ifc/09.ifc",
+	},
+	{
+	  name: "Project 10",
+	  id: "10",
+	  url: "../ifc/10.ifc",
+	},
+  ];
 
 /**
  * @license
